bdce7f39a9536cce5b14009fb20d17fe7bd13346
Avidity <2642350585@qq.com>
2026-05-14 20:10:35 +0800

    feat: 初始化 React + Vite + TypeScript 项目

---
+++ seckill-frontend-react/src/routes/index.tsx
@@ -0,0 +1,11 @@
+import { Routes, Route } from 'react-router-dom';
+
+function AppRoutes() {
+  return (
+    <Routes>
+      <Route path="/" element={<div>秒杀系统</div>} />
+    </Routes>
+  );
+}
+
+export default AppRoutes;
