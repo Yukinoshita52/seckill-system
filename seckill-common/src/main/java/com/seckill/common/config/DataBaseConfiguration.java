b347bd3d72de8d5649f124f17ec55d2c0502fbd7
Avidity <2642350585@qq.com>
2026-05-14 16:37:09 +0800

    feat: 添加 BlockAttackInnerInterceptor 防全表更新/删除

--- seckill-common/src/main/java/com/seckill/common/config/DataBaseConfiguration.java
+++ seckill-common/src/main/java/com/seckill/common/config/DataBaseConfiguration.java
@@ -3,6 +3,7 @@ package com.seckill.common.config;
 import com.baomidou.mybatisplus.annotation.DbType;
 import com.baomidou.mybatisplus.core.handlers.MetaObjectHandler;
 import com.baomidou.mybatisplus.extension.plugins.MybatisPlusInterceptor;
+import com.baomidou.mybatisplus.extension.plugins.inner.BlockAttackInnerInterceptor;
 import com.baomidou.mybatisplus.extension.plugins.inner.PaginationInnerInterceptor;
 import java.time.LocalDateTime;
 import org.apache.ibatis.reflection.MetaObject;
@@ -15,8 +16,8 @@ public class DataBaseConfiguration {
   @Bean
   public MybatisPlusInterceptor mybatisPlusInterceptor() {
     MybatisPlusInterceptor interceptor = new MybatisPlusInterceptor();
-    // todo: 添加防全表更新/删除插件（BlockAttackInnerInterceptor）；如需乐观锁，添加 OptimisticLockerInnerInterceptor
     interceptor.addInnerInterceptor(new PaginationInnerInterceptor(DbType.MYSQL));
+    interceptor.addInnerInterceptor(new BlockAttackInnerInterceptor());
     return interceptor;
   }
 
